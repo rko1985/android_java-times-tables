27c84225f26a16c3c7d9a0a2b6adeed5a419bc90
rko1985 <35703264+rko1985@users.noreply.github.com>
2020-02-12 09:19:37 -1000

    output times table to list view

--- app/src/main/java/com/myappcompany/russell/timestables/MainActivity.java
+++ app/src/main/java/com/myappcompany/russell/timestables/MainActivity.java
@@ -2,17 +2,72 @@ package com.myappcompany.russell.timestables;
 
 import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
+import android.util.Log;
+import android.widget.ArrayAdapter;
 import android.widget.ListView;
 import android.widget.SeekBar;
 
+import java.util.ArrayList;
+
 public class MainActivity extends AppCompatActivity {
+
+    ListView timesTablesListView;
+
+    public void generateTimesTable (int timesTableNumber){
+        ArrayList<String> timesTableContent = new ArrayList<String>();
+
+        for(int j = 1; j <= 10; j++){
+            timesTableContent.add(Integer.toString(j * timesTableNumber));
+        }
+
+        ArrayAdapter<String> arrayAdapter = new ArrayAdapter<String>(this, android.R.layout.simple_list_item_1, timesTableContent);
+        timesTablesListView.setAdapter(arrayAdapter);
+    }
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
 
-        SeekBar timesTablesSeekBar = findViewById(R.id.timesTablesSeekBar);
-        ListView timesTablesListView = findViewById(R.id.timesTablesListView);
+        final SeekBar timesTablesSeekBar = findViewById(R.id.timesTablesSeekBar);
+        timesTablesListView = findViewById(R.id.timesTablesListView);
+
+        int max = 20;
+        int startingPosition = 10;
+
+        timesTablesSeekBar.setMax(max);
+        timesTablesSeekBar.setProgress(10);
+
+        generateTimesTable(startingPosition);
+
+        timesTablesSeekBar.setOnSeekBarChangeListener(new SeekBar.OnSeekBarChangeListener() {
+            @Override
+            public void onProgressChanged(SeekBar seekBar, int i, boolean b) {
+
+                int min = 1;
+                int timesTableNumber;
+
+                if (i < min){
+                    timesTableNumber = min;
+                    timesTablesSeekBar.setProgress(min);
+                } else {
+                    timesTableNumber = i;
+                }
+
+                Log.i("Seekbar Value", Integer.toString(timesTableNumber));
+                generateTimesTable(timesTableNumber);
+
+            }
+
+            @Override
+            public void onStartTrackingTouch(SeekBar seekBar) {
+
+            }
+
+            @Override
+            public void onStopTrackingTouch(SeekBar seekBar) {
+
+            }
+        });
     }
 }
