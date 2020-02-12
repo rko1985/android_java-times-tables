20bb275f2e6c158bdbc8334ca9c20776d17655a0
rko1985 <35703264+rko1985@users.noreply.github.com>
2020-02-12 08:31:14 -1000

    setting up layout

--- app/src/main/java/com/myappcompany/russell/timestables/MainActivity.java
+++ app/src/main/java/com/myappcompany/russell/timestables/MainActivity.java
@@ -2,6 +2,8 @@ package com.myappcompany.russell.timestables;
 
 import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
+import android.widget.ListView;
+import android.widget.SeekBar;
 
 public class MainActivity extends AppCompatActivity {
 
@@ -9,5 +11,8 @@ public class MainActivity extends AppCompatActivity {
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
+
+        SeekBar timesTablesSeekBar = findViewById(R.id.timesTablesSeekBar);
+        ListView timesTablesListView = findViewById(R.id.timesTablesListView);
     }
 }
